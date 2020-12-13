59d7fa872f666e24bb42b6a574fc489903606032
BassProulx <68520323+BassProulx@users.noreply.github.com>
2020-12-13 15:40:54 -0500

    Plot Fixed-Effect Design Matrix and adjust contrast of design matrix plot

--- neuroimaging/runFit.m
+++ neuroimaging/runFit.m
@@ -1,6 +1,6 @@
 function runFit
 
-actuallyRun = 1;
+actuallyRun = 0;
 noMovement = 1;
 saveFig = 1;
 exclude = 1;
@@ -49,7 +49,7 @@ if ~actuallyRun
     subjInd = 1;
     
     % Random Effect Model
-    f = figure();
+    f = figure('WindowStyle','docked');
     
     subplot(1,16+25,25+(1:16))
     if noMovement
@@ -59,7 +59,11 @@ if ~actuallyRun
         load(fullfile(repo,funDir,outDir,subjList{subjInd},'v1SinCos_1perRun_move12.mat'),'results')
         run1 = results.OLS.mixed.designmatrix(:,[1 2 67:80]);
     end
-    imagesc(run1(any(run1,2),:)); colormap gray
+    %scale for max contrast matix
+    tmp = run1(any(run1,2),:);
+    tmp(:,3) = max(tmp(:,1));
+    tmp(:,4) = tmp(:,4)./max(abs(tmp(:,4)));
+    imagesc(tmp); colormap gray
     title('Sinusoidal Response');
     ax1 = gca; ax1.XTick = []; ax1.YTick = [];
 %     xlabel('Regressors');
@@ -84,7 +88,9 @@ if ~actuallyRun
         tmp = load(fullfile(repo,funDir,outDir,subjList{1},'v1resp_1perRun_move12_resp.mat'));
         run1 = tmp.results.OLS.mixed.designmatrix(:,[1:12 397:409]);
     end
-    imagesc(run1(any(run1,2),:)); colormap gray
+    tmp = run1(any(run1,2),:);
+    tmp(:,end) = tmp(:,end)./max(abs(tmp(:,end)));
+    imagesc(tmp); colormap gray
     title('Model-Free Response');
     ax2 = gca; ax2.XTick = []; ax2.YTick = [];
 %     xlabel('Regressors');% ylabel('TRs');
@@ -102,11 +108,28 @@ if ~actuallyRun
     ax1.XAxis.FontSize = ax1.XAxis.FontSize*0.8;
     ax2.XAxis.FontSize = ax2.XAxis.FontSize*0.8;
     
+    ax = [ax1 ax2];
+    for i = 1:length(ax)
+        curAxe = ax(i);
+        curAxeB = axes();
+        linkprop([curAxe curAxeB],'position'); linkprop([curAxe curAxeB],'XLim');
+        curAxeB.Color = 'none'; curAxeB.Box = 'on';
+        curAxeB.XTick = []; curAxeB.YTick = [];
+%         delta = mode(diff(curAxe.XTick));
+%         curAxeB.XTick = [curAxe.XTick curAxe.XTick(end)+delta]-delta/2;
+%         curAxeB.TickLength = [0 0];
+%         curAxeB.XTickLabel = [];
+%         curAxeB.XGrid = 'on';
+%         curAxeB.GridAlpha = 1;
+    end
+    if saveFig
+        saveas(f,fullfile(repo,funDir,outDir,'designMatrices_randomEffect'))
+        disp(fullfile(repo,funDir,outDir,'designMatrices_randomEffect.fig'))
+    end
+    
     
     % Fixed Effect model
-    error('Code the Fixed Effect Model figure here')
-    f = figure();
-    ind = results.OLS.fixed.designmatrixSessInd==1;
+    f = figure('WindowStyle','docked');
     if noMovement
         load(fullfile(repo,funDir,outDir,subjList{subjInd},'v1SinCos_1perRun.mat'),'results')
 %         run1 = results.OLS.fixed.designmatrix(:,[1 2 67:68]);
@@ -114,32 +137,58 @@ if ~actuallyRun
         load(fullfile(repo,funDir,outDir,subjList{subjInd},'v1SinCos_1perRun_move12.mat'),'results')
 %         run1 = results.OLS.fixed.designmatrix(:,[1 2 67:80]);
     end
-    run1 = results.OLS.fixed.sess1.designMatrixFull;
+    sessInd = results.OLS.fixed.designmatrixSessInd==1;
+    run1 = results.OLS.fixed.designmatrix(sessInd,:);
+    runOrderLabel = results.OLS.fixed.designmatrixRunInd(sessInd,:);
+    run1 = run1(:,~all(run1==0,1));
+    runOrderLabelList = unique(runOrderLabel);
+    tmp = [];
+    for i = 1:length(runOrderLabelList)
+        trInd = runOrderLabel==runOrderLabelList(i);
+        tmp = cat(1,tmp,run1(trInd,:));
+    end
+    tmp(:,7:end) = run1(:,7:end);
+    run1 = tmp;
+    tmp = run1(:,7:2:end);
+    run1(:,7:2:end) = run1(:,7:2:end)./mode(tmp(tmp~=0));
+    tmp = run1(:,8:2:end);
+    run1(:,8:2:end) = run1(:,8:2:end)./max(abs(tmp(tmp~=0)));
+    
     imagesc(run1); colormap gray
-    imagesc(results.OLS.fixed.designmatrixPieces.cond)
-    imagesc(results.OLS.fixed.designmatrix); colormap gray
-    imagesc(run1(any(run1,2),:)); colormap gray
-    title('Sinusoidal Response');
+    title({'Fixed-Effect Model' '\fontsize{8}Sinusoidal Response'});
     ax1 = gca; ax1.XTick = []; ax1.YTick = [];
 %     xlabel('Regressors');
     ylabel('TRs');
-    ax1.XTick = 1:size(run1,2);
+    ax1.XTick = [1.5 3.5 5.5];
     ax1.Box = 'off';
     ax1.TickDir = 'out';
-    ax1.XTickLabel = {'sin' 'cos' 'constant' 'drift' 'x' 'y' 'z' 'pitch' 'roll' 'yaw' 'x''' 'y''' 'z''' 'pitch''' 'roll''' 'yaw'''};
+    ax1.XTickLabel = {'ori1' 'ori2' 'plaid'};
     ax1.XTickLabelRotation = -90;
     ax1.YAxis.Color = 'none';
     ax1.YAxis.Label.Visible = 'on';
     ax1.YAxis.Label.Color = 'k';
     
-    xVal = diff(ax1.XLim);
-    xWidth = ax1.Position(3);
-    
-    
+    ax = [ax1];
+    for i = 1:length(ax)
+        curAxe = ax(i);
+        curAxeB = axes();
+        linkprop([curAxe curAxeB],'position'); linkprop([curAxe curAxeB],'XLim');
+        curAxeB.Color = 'none'; curAxeB.Box = 'on';
+        curAxeB.XTick = []; curAxeB.YTick = [];
+%         delta = mode(diff(curAxe.XTick));
+%         curAxeB.XTick = [curAxe.XTick curAxe.XTick(end)+delta]-delta/2;
+%         curAxeB.TickLength = [0 0];
+%         curAxeB.XTickLabel = [];
+%         curAxeB.XGrid = 'on';
+%         curAxeB.GridAlpha = 1;
+    end
+    curAxeB.XTick = mean([6.5 size(run1,2)+0.5]);
+    curAxeB.XTickLabel = {'constant+drift for each run'};
+    curAxeB.TickDir = 'out';
     
     if saveFig
-        saveas(f,fullfile(repo,funDir,outDir,'designMatrices'))
-        disp(fullfile(repo,funDir,outDir,'designMatrices.fig'))
+        saveas(f,fullfile(repo,funDir,outDir,'designMatrices_fixedEffect'))
+        disp(fullfile(repo,funDir,outDir,'designMatrices_fixedEffect.fig'))
     end
     return
 end
