bdd2cc8fc399b7446e11dde2c06aa0f1a95d9324
Proulx-S <sebastien.proulx2@mail.mcgill.ca>
2022-05-11 21:08:46 -0400

    fool around

--- HRdelay.m
+++ HRdelay.m
@@ -2,36 +2,15 @@
 clear all
 close all
 
+%% Initiate script
 p.paths.pwd = pwd;
 p.paths.data = ''; % when empty, data folder defaults to pwd/../HRdelay (to keep data and code separate)
 p.info.dataURL = 'https://zenodo.org/record/5192849';
 p.info.subjList = {'02jp' '03sk' '04sp' '05bm' '06sb' '07bj'}';
 addpath(genpath(fullfile(p.paths.pwd,'fun')))
 % Get data from data repository
-getRepoData(p)
-
+p = getRepoData(p);
 return
-
-%%
-p.info.anaId = 'myAna';
-
-
-
-
-
-return
-
-% data() = load(fullfile(p.paths.data,'03sk.mat'))
-
-
-urlwrite('https://zenodo.org/record/5192849/files/02jp.mat?download=1',fullfile(p.paths.data,'test.mat'))
-urlwrite('https://zenodo.org/record/5192849/files/03sk.mat?download=1',fullfile(p.paths.data,'03sk.mat'))
-% data() = load(fullfile(p.paths.data,'03sk.mat'))
-data = webread('https://zenodo.org/record/5192849/files/02jp.mat?download=1');
-
-p.paths.data = fullfile('McGill University/Farivar Lab - Dissertations/Sebastien/Manuscripts/aa - in preparation/SP_Neuroimage_HRdelay',p.anaID); if ~exist(p.paths.repo.out,'dir'); mkdir(p.paths.repo.out); end
-
-
 % p.anaID = '2021-06-15_allData';
 % finalSubDir = p.anaID;
 % if ~ispc
@@ -41,8 +20,9 @@ p.paths.data = fullfile('McGill University/Farivar Lab - Dissertations/Sebastien
 % end
 % p.paths.repo.out = fullfile('McGill University/Farivar Lab - Dissertations/Sebastien/Manuscripts/aa - in preparation/SP_Neuroimage_HRdelay',p.anaID); if ~exist(p.paths.repo.out,'dir'); mkdir(p.paths.repo.out); end
 
-
-%% Display parameters
+%% Define analysis
+p.info.anaId = 'myAna';
+% Display parameters
 % figOption.save = 1; % save all figures
 % figOption.subj = 2; % subjInd-> plots participants subjInd; +inf-> plots all participant (if verbose==0, will only plot subjInd==1 but still produce and save all the other figures)
 p.figOption.verbose  = 2;
@@ -57,11 +37,11 @@ p.termOption.verbose = 1;
 p.termOption.save = 0;
 p.termOption.finalDir = fullfile(p.paths.pwd,p.info.anaId,'matlabTermOutputs'); if ~exist(p.figOption.finalDir,'dir'); mkdir(p.figOption.finalDir); end
 
-%% Permutation
+% Permutation
 p.perm.doIt = 1;
 p.perm.n = 2^13;
 
-%% Bootstrapping
+% Bootstrapping
 p.boot.n = 2^13;
 
 %% Open diary
@@ -75,78 +55,89 @@ if p.termOption.verbose && p.termOption.save
     disp(datestr(now))
 end
 
-%% Input Data
-p.paths.repo.in = 'C:\Users\sebas\OneDrive - McGill University\dataBig';
+% %% Input Data
+% p.paths.repo.in = 'C:\Users\sebas\OneDrive - McGill University\dataBig';
 
-%% Dependencies
-gitDependencyPath = fullfile(p.paths.home,'Documents/GitHub/utilities');
-addpath(genpath(fullfile(gitDependencyPath,'bassFun/matlab')));
-addpath(genpath(fullfile(gitDependencyPath,'circstat-matlab')));
-addpath(genpath(fullfile(gitDependencyPath,'RAMBiNo')));
-addpath(genpath(fullfile(gitDependencyPath,'BrewerMap')));
-matDependencyPath = fullfile(p.paths.home,'OneDrive - McGill University\MATLAB');
-addpath(genpath(fullfile(matDependencyPath,'shadedErrorBar-master')));
-addpath(genpath(fullfile(matDependencyPath,'InterX')))
-addpath(genpath(fullfile(pwd,'matlabFun')));
+% %% Dependencies
+% gitDependencyPath = fullfile(p.paths.home,'Documents/GitHub/utilities');
+% addpath(genpath(fullfile(gitDependencyPath,'bassFun/matlab')));
+% addpath(genpath(fullfile(gitDependencyPath,'circstat-matlab')));
+% addpath(genpath(fullfile(gitDependencyPath,'RAMBiNo')));
+% addpath(genpath(fullfile(gitDependencyPath,'BrewerMap')));
+% matDependencyPath = fullfile(p.paths.home,'OneDrive - McGill University\MATLAB');
+% addpath(genpath(fullfile(matDependencyPath,'shadedErrorBar-master')));
+% addpath(genpath(fullfile(matDependencyPath,'InterX')))
+% addpath(genpath(fullfile(pwd,'matlabFun')));
 % Matlab toolboxes:
 % -Image Processing
 % -Statistics and Machine Learning
 % -Mapping
 % -Curve Fitting
 % -Bioinformatics
-verbose = 1; % prints more info
+% verbose = 1; % prints more info
 
-%% Meta data
-p.meta.subjList = {'02jp' '03sk' '04sp' '05bm' '06sb' '07bj'}';
+% %% Meta data
+% p.meta.subjList = {'02jp' '03sk' '04sp' '05bm' '06sb' '07bj'}';
 
 
-%% Feature selection parameters
-% Within visual field region of the stimulus
+% Feature selection parameters
+% within visual field region of the stimulus
 p.featSel.fov.doIt = 1;
 p.featSel.fov.threshMethod = 'empirical'; % 'empirical' 'ecc'
 p.featSel.fov.areaLabel = 'v1';
 p.featSel.fov.threshVal = [0.75 7]; % threshMethod='ecc'
 p.featSel.fov.percentile = 20; % threshMethod='ecc'
-% Activated voxels (fixed-effect)
+% activated voxels (fixed-effect)
 p.featSel.act.doIt = 0;
 p.featSel.act.threshMethod = '%ile'; % '%ile' 'p' 'fdr'
 p.featSel.act.threshVal = 0.05; % threshMethod='p' or 'fdr'
 p.featSel.act.percentile = 20; % threshMethod='%ile'
-% Activated voxels (random-effect)
+% activated voxels (random-effect)
 p.featSel.respVecSig.doIt = 1;
 p.featSel.respVecSig.threshMethod = 'p'; % '%ile' 'p' 'fdr'
 p.featSel.respVecSig.threshVal = 0.05; % threshMethod='p' or 'fdr'
 p.featSel.respVecSig.percentile = 20; % threshMethod='%ile'
-% Less likely-to-be-vein voxels
+% less likely-to-be-vein voxels
 p.featSel.vein.doIt = 1;
 p.featSel.vein.threshMethod = '%ile'; % '%ile'
 p.featSel.vein.threshVal = 0.01; % not used
 p.featSel.vein.percentile = 20; % threshMethod='%ile'
-% Most discriminant voxels
+% most discriminant voxels
 p.featSel.respVecDiff.doIt = 1;
 p.featSel.respVecDiff.threshMethod = '%ile'; % '%ile' 'p' 'fdr'
 p.featSel.respVecDiff.threshVal = 0.5; % threshMethod='p' or 'fdr'
 p.featSel.respVecDiff.percentile = 20; % threshMethod='%ile'
-% Feature Combination
+% feature Combination
 p.featSel.global.doIt = 1;
 p.featSel.global.method = 'allData';
 % 'allData'-> featSel uses all three conditions, irrespective of the condition pairs to be decoded
 % 'custom1'-> featSel of active voxels uses all three conditions but featSel of discriminant voxels uses only the conditions to be decoded
 % 'custom2'-> featSel of active and most discriminant voxels uses only the conditions to be decoded
 
-%% SVM parameters
+% SVM parameters
 p.svm.kernel.type = 'lin';
 p.svm.complexSpace = 'bouboulisDeg1'; % 'bouboulisDeg1' 'bouboulisDeg2'
 p.svm.doWithin = 0;
 
-%% Channel parameters
+% Channel parameters
 p.svm.condPairList = {'grat1VSgrat2' 'grat1VSplaid' 'grat2VSplaid'};
 % p.svm.respFeatList = {'cartNoDelay' 'delay' 'cart'};
 p.svm.respFeatList = {'delay' 'cartNoDelay' 'cart'};
 
 
+for subjInd = 1:length(p.info.subjList)
+    data = load(fullfile(p.paths.data,[p.info.subjList{subjInd} '.mat']))
+%     fun = mean(data.d.fun(1).data{1},4);
+    fun = zeros(size(data.d.anat.brain,1:3));
+    fun(data.d.anat.roiMask) = mean(data.d.fun(1).data{1},4);
+    imagesc(fun(:,:,10))
+end
+
+dir(p.paths.data)
+
+return
 if 0
-    importData(verbose)
+    importData(p)
     applyAreaMask(figOption)
     %     processWaveletResponses(figOption,verbose)
 end
--- fun/getRepoData.m
+++ fun/getRepoData.m
@@ -1,4 +1,4 @@
-function getRepoData(p)
+function p = getRepoData(p)
 
 if isempty(p.paths.data)
     [a,b,~] = fileparts(p.paths.pwd);
@@ -8,11 +8,11 @@ end
 if ~exist(p.paths.data,'dir'); mkdir(p.paths.data); end
 data = repmat(struct,length(p.info.subjList),1);
 disp(['Getting data' newline 'from: ' p.info.dataURL newline 'to:   ' p.paths.data])
-for subjInd = 1:length(p.info.subjList)
-    if ~exist(char(fullfile(p.paths.data,p.info.subjList(subjInd))),'file')
+for subjInd = 1:2%length(p.info.subjList)
+    if ~exist(fullfile(p.paths.data,[p.info.subjList{subjInd} '.mat']),'file')
         disp(['Downloading subj ' p.info.subjList{subjInd}])
         url = [p.info.dataURL '/files/' p.info.subjList{subjInd} '.mat?download=1'];
-        urlwrite(url,fullfile(p.paths.data,p.info.subjList{subjInd}));
+        urlwrite(url,fullfile(p.paths.data,[p.info.subjList{subjInd} '.mat']));
     else
         disp([p.info.subjList{subjInd} ' already downloaded'])
     end
